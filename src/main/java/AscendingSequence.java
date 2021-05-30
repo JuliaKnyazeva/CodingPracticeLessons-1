1d16b58d4b9fd49cb61528a0f3230680bdb24a45
JuliaKnyazeva <jknyazeva@yandex.ru>
2021-05-30 16:29:31 +0300

    Update AscendingSequence.java

--- src/main/java/AscendingSequence.java
+++ src/main/java/AscendingSequence.java
@@ -1,6 +1,31 @@
 public class AscendingSequence {
 
-    public int[] buildAscendingArray (int startNumber, int endNumber) {
+   public int[] buildAscendingArray (int startNumber, int endNumber) {
+        int[] array = new int[endNumber - startNumber +1];
+
+        for(int i = 0; i < endNumber - startNumber +1; i++) {
+            array[i] = startNumber + i;
+            System.out.println(array[i]);
+        }
+
+        return array;
+
+    }
+
+    public String buildAscendingString() {
+
+        StringBuilder result = new StringBuilder();
+        for(int i = 1; i < 5; i++) {
+            result.append(i).append(", ");
+            result.toString();
+        }
+        result.append(5);
+        System.out.println(result);
+        return String.valueOf(result);
+
+    }
+
+    public int[] buildAscendingArray2 (int startNumber, int endNumber) {
         int[] array = new int[endNumber - startNumber + 1];
 
         for (int i = 0; i < endNumber - startNumber + 1; i ++) {
@@ -9,5 +34,36 @@ public class AscendingSequence {
         }
 
         return array;
+    }
+
+    //конкатенация строк "а" + "б"
+    //String.format()
+    //String.concat()
+    //StringBuilder
+
+    public String buildAscendingString(int startNumber, int endNumber) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (startNumber < endNumber && startNumber >= Integer.MIN_VALUE && endNumber <= Integer.MAX_VALUE) {
+            for (int i = startNumber; i < endNumber; i ++) {
+                stringBuilder.append(i).append(", ");
+            }
+            stringBuilder.append(endNumber);
+            System.out.println(stringBuilder);
+
+            return String.valueOf(stringBuilder);
+        } else {
+
+            return "Error";
+        }
+    }
+
+    public List<Integer> buildAscendingList(int startNumber, int endNumber) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = startNumber; i <= endNumber; i++) {
+            list.add(i);
+        }
+        return list;
     }
 }
